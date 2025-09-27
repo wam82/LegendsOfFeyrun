378bafbd721f957c0e5edfe9b6954da147e8a96b
wam82 <monsterwam8282@gmail.com>
2025-09-27 14:49:01 -0400

    Character mesh import and set up

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Character
+{
+    public class Character : MonoBehaviour
+    {
+        [Header("Character Attributes")]
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float rotationSpeed;
+        // [SerializeField]  private float jumpForce;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
