cd72ae6e102ea7430f1cac847adc2e64e9c2eba5
wam82 <monsterwam8282@gmail.com>
2025-09-28 08:06:56 -0400

    Moved player movement to individual method and re-structured the character model hierarchy

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -21,16 +21,7 @@ namespace Character
             _inputVector = inputVector;
         }
 
-        private void Awake()
-        {
-            _characterController =  GetComponent<CharacterController>();
-            if (_characterController == null)
-            {
-                Debug.LogError("Character controller not found");
-            }
-        }
-
-        private void Update()
+        private void MoveCharacter()
         {
             // 1. Get camera's forward and right, ignoring vertical component
             Vector3 camForward = cameraTransform.forward;
@@ -39,9 +30,9 @@ namespace Character
             camRight.y = 0f;
             camForward.Normalize();
             camRight.Normalize();
-
+            
             // 2. Compute movement direction from input
-            Vector3 desiredDirection = camForward * _inputVector.y + camRight * _inputVector.x;;
+            Vector3 desiredDirection = camForward * _inputVector.y + camRight * _inputVector.x;
             
             // 3. Rotate character to face movement direction
             if (desiredDirection.sqrMagnitude > 0.01f)
@@ -51,6 +42,20 @@ namespace Character
             }
             
             _characterController.Move(desiredDirection * (movementSpeed * Time.deltaTime));
+        }
+
+        private void Awake()
+        {
+            _characterController =  GetComponent<CharacterController>();
+            if (_characterController == null)
+            {
+                Debug.LogError("Character controller not found");
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            MoveCharacter();
         }
     }
 }
