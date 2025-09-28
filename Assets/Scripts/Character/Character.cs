6c8692a90e1e8eda19eab790a3aa145969f47760
wam82 <monsterwam8282@gmail.com>
2025-09-28 07:32:47 -0400

    Player movement base

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Character
@@ -7,18 +8,49 @@ namespace Character
         [Header("Character Attributes")]
         [SerializeField] private float movementSpeed;
         [SerializeField] private float rotationSpeed;
-        // [SerializeField]  private float jumpForce;
         
-        // Start is called before the first frame update
-        void Start()
+        [Header("Character Components")]
+        [SerializeField] private Transform cameraTransform;
+        
+        private CharacterController _characterController;
+        
+        private Vector2 _inputVector;
+        
+        public void SetInputVector(Vector2 inputVector)
         {
-        
+            _inputVector = inputVector;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Awake()
         {
-        
+            _characterController =  GetComponent<CharacterController>();
+            if (_characterController == null)
+            {
+                Debug.LogError("Character controller not found");
+            }
+        }
+
+        private void Update()
+        {
+            // 1. Get camera's forward and right, ignoring vertical component
+            Vector3 camForward = cameraTransform.forward;
+            Vector3 camRight = cameraTransform.right;
+            camForward.y = 0f;
+            camRight.y = 0f;
+            camForward.Normalize();
+            camRight.Normalize();
+
+            // 2. Compute movement direction from input
+            Vector3 desiredDirection = camForward * _inputVector.y + camRight * _inputVector.x;;
+            
+            // 3. Rotate character to face movement direction
+            if (desiredDirection.sqrMagnitude > 0.01f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(desiredDirection);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            }
+            
+            _characterController.Move(desiredDirection * (movementSpeed * Time.deltaTime));
         }
     }
 }
--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,20 +1,53 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Character
 {
     public class PlayerController : MonoBehaviour
     {
+        private PlayerInput _playerInput;
+        private Character _character;
         
-        // Start is called before the first frame update
-        void Start()
+        public void OnMove(InputAction.CallbackContext context)
         {
-        
+            Debug.Log("OnMove called");
+            if (_character != null)
+            {
+                _character.SetInputVector(context.ReadValue<Vector2>());
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Awake()
         {
-        
+            _playerInput =  GetComponent<PlayerInput>();
+            if (_playerInput == null)
+            {
+                Debug.LogError("No player input found");
+            }
+            
+            _playerInput.actions.Enable();
+            
+            _character =  GetComponent<Character>();
+            if (_character == null)
+            {
+                Debug.LogError("No character found");
+            }
+        }
+
+        private void Start()
+        {
+            InputActionMap map = _playerInput.actions.FindActionMap("Game");
+
+            // Enable the action map
+            if (map != null)
+            {
+                map.Enable();
+            }
+            else
+            {
+                Debug.LogError("Action Map 'Game' not found!");
+            }
         }
     }
 }
